619768ca441acb5caf4afdee404403c2b8a62d05
Katherine Smith <catblack101420cb@gmail.com>
2026-04-21 15:17:12 -0700

    Realizing that I couldn't just play again, I updated the program to be in a while loop and the ability to play again.

--- hangman_game.py
+++ hangman_game.py
@@ -44,49 +44,68 @@ class HangmanGame:
                 display += "_ "
 
         return display.strip()
+    
+    # Resets the game state for a new round
+    def __reset_game(self):
+        self.__word = self.__choose_word()
+        self.__correct_letters = set()
+        self.__wrong_letters = set()
+        self.__wrong_guesses = 0
 
     # This is the start of the game here.
     def start_game(self):
         print("Welcome to Hangman!")
         print("(Hint: It's boardgame themed!)")
 
-        # Until the max attempts are made, the user can continue to guess letters.
-        while self.__wrong_guesses < self.__max_attempts:
-            print("\nWord:", self.__display_word())
-            print("Correct letters:", sorted(self.__correct_letters))
-            print("Wrong letters:", sorted(self.__wrong_letters))
-            print("Attempts left:", self.__max_attempts - self.__wrong_guesses)
+        # Starts a game loop
+        while True:  # Game loop (for replay)
+            self.__reset_game()
 
-            guess = input("Guess a letter: ").lower()
+            # Until the max attempts are made, the user can continue to guess letters.
+            while self.__wrong_guesses < self.__max_attempts:
+                print("\nWord:", self.__display_word())
+                print("Correct letters:", sorted(self.__correct_letters))
+                print("Wrong letters:", sorted(self.__wrong_letters))
+                print("Attempts left:", self.__max_attempts - self.__wrong_guesses)
 
-            # Makes sure that a letter is being inputted.
-            if len(guess) != 1 or not guess.isalpha():
-                print("Please enter a single letter.")
-                continue
+                guess = input("Guess a letter: ").lower()
 
-            # Makes sure the letter hasn't been asked.
-            if guess in self.__correct_letters or guess in self.__wrong_letters:
-                print("You already guessed that letter.")
-                continue
+                # Makes sure that a letter is being inputted.
+                if len(guess) != 1 or not guess.isalpha():
+                    print("Please enter a single letter.")
+                    continue
 
-            # Checks if the guess is correct
-            if guess in self.__word:
-                print("Correct guess!")
-                self.__correct_letters.add(guess)
-            else:
-                print("Wrong guess!")
-                self.__wrong_letters.add(guess)
-                self.__wrong_guesses += 1
+                # Makes sure the letter hasn't been asked.
+                if guess in self.__correct_letters or guess in self.__wrong_letters:
+                    print("You already guessed that letter.")
+                    continue
 
-            # Checks win condition
-            if all(letter in self.__correct_letters for letter in self.__word):
-                print("\nWord:", self.__word)
-                print("Congratulations! You guessed the word!")
-                return
+                # Checks if the guess is correct
+                if guess in self.__word:
+                    print("Correct guess!")
+                    self.__correct_letters.add(guess)
+                else:
+                    print("Wrong guess!")
+                    self.__wrong_letters.add(guess)
+                    self.__wrong_guesses += 1
 
-        # Without any attempts left, the user lost the game
-        print("\nGame Over! You ran out of attempts.")
-        print("The word was:", self.__word)
+                # Checks win condition
+                if all(letter in self.__correct_letters for letter in self.__word):
+                    print("\nWord:", self.__word)
+                    print("Congratulations! You guessed the word!")
+                    break
+
+            # Without any attempts left, the user lost the game
+            if self.__wrong_guesses >= self.__max_attempts:
+                print("\nGame Over! You ran out of attempts.")
+                print("The word was:", self.__word)
+
+            # Asks to play again
+            play_again = input("\nDo you want to play again? (y/n): ").lower()
+
+            if play_again != "y":
+                print("Thanks for playing!")
+                break
 
 # Runs the game 
 if __name__ == "__main__": 
