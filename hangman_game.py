141e4c58756b7a96a8636b852fe5fd5f729a352c
Katherine Smith <catblack101420cb@gmail.com>
2026-04-21 14:58:33 -0700

    I've added a visual for the word being guessed. I've also separted wrong and right letters to not confuse the user.

--- hangman_game.py
+++ hangman_game.py
@@ -25,7 +25,8 @@ class HangmanGame:
     def __init__(self):
         # Private attribute for the selected word
         self.__word = self.__choose_word()
-        self.__guessed_letters = set()
+        self.__correct_letters = set()
+        self.__wrong_letters = set()
         self.__wrong_guesses = 0
         self.__max_attempts = 100  # Temporary for testing win condition, later will be 9
 
@@ -33,12 +34,28 @@ class HangmanGame:
     def __choose_word(self):
         return random.choice(self.hangman_word_bank)
     
+    # Shows the selected word with underscores for missing letters
+    def __display_word(self):
+        display = ""
+        for letter in self.__word:
+            if letter in self.__correct_letters:
+                display += letter + " "
+            else:
+                display += "_ "
+
+        return display.strip()
+
     # This is the start of the game here.
     def start_game(self):
         print("Welcome to Hangman!")
+        print("(Hint: It's boardgame themed!)")
 
         # Until the max attempts are made, the user can continue to guess letters.
         while self.__wrong_guesses < self.__max_attempts:
+            print("\nWord:", self.__display_word())      # Shows the progress of the word
+            print("Correct letters:", sorted(self.__correct_letters))  # Shows the correct letters guessed
+            print("Wrong letters:", sorted(self.__wrong_letters))   # Shows the incorrect letters guessed
+        
             guess = input("Guess a letter: ").lower()
 
             # Makes sure that a letter is being inputted.
@@ -47,34 +64,28 @@ class HangmanGame:
                 continue
 
             # Makes sure the letter hasn't been asked.
-            if guess in self.__guessed_letters:
+            if guess in self.__correct_letters or guess in self.__wrong_letters:
                 print("You already guessed that letter.")
                 continue
-
-            # Add guess to the set
-            self.__guessed_letters.add(guess)
 
             # Checks if the guess is correct
             if guess in self.__word:
                 print("Correct guess!")
+                self.__correct_letters.add(guess)
             else:
                 print("Wrong guess!")
+                self.__wrong_letters.add(guess)
                 self.__wrong_guesses += 1
 
-            # TEMP: Shows progress
-            print("Guessed letters:", self.__guessed_letters)
-
-            # TEMP: Reveals word for testing
-            print("Word (for now):", self.__word)
-
-            # Checks for win condition
-            if all(letter in self.__guessed_letters for letter in self.__word):
-                print("You guessed the word!")
+            # Check win condition
+            if all(letter in self.__correct_letters for letter in self.__word):
+                print("\nWord:", self.__word)
+                print("Congratulations! You guessed the word!")
                 break
 
         print("Game ended.")
         
-        
+
 # Runs the game
 if __name__ == "__main__":
     game = HangmanGame()
